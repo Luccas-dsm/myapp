32c08be366432e1ffd18f705a798bede4368fa2d
luccasdsm <luccasdsm@hotmail.com>
2022-06-30 16:26:45 -0300

    v1

---
+++ src/components/Aside/Styles.ts
@@ -0,0 +1,5 @@
+import styled, { StyledFunction } from "styled-components";
+
+ export const Container = styled.div ``;
+
+  
---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+export default createGlobalStyle`
+
+   * {
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+   }
+   *,button,input{
+    border: none;
+    font-family: 'Roboto', sans-serif;
+    cursor: pointer;
+    outline: 0;
+}
+   html,border-style,#root{
+    height:100%;
+   }
+
+`;
